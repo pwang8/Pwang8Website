a41ea26d7987e2470fa6c440dc676894b57ad309
Paul Wang <p.wang8@outlook.com>
2018-09-12 15:36:29 -0600

    initial redesign, added landing page and started reformatting page to a sidebar+content view

--- js/main.js
+++ js/main.js
@@ -2,27 +2,34 @@
  *    credits to: https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
  */
 $(document).ready(function(){
-// Add smooth scrolling to all links
-$("a").on('click', function(event) {
+      // Add smooth scrolling to all links
+      $("a").on('click', function(event) {
 
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+            // Store hash
+            var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      //Changed the time 800ms to 600ms
-      $('html, body').animate({
-      scrollTop: $(hash).offset().top
-      }, 600, function(){
-      
-      // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            //Changed the time 800ms to 600ms
+            $('html, body').animate({
+            scrollTop: $(hash).offset().top
+            }, 600, function(){
+            
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+            });
+            } // End if
       });
-      } // End if
 });
-});
+
+/**   Otherwise people won't see my beautiful landing page on refresh :(
+ * 
+ */
+window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+    }
