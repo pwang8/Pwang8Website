82f45b1d8395ba70af3d6b5881e05b1bfe3f6b79
Paul Wang <p.wang8@outlook.com>
2021-04-23 19:47:05 -0600

    Added scoring system

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -2,7 +2,17 @@ import React, { Component } from "react";
 import Matter from "matter-js";
 
 class Project extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            score: 0
+        }
+    }
+
     componentDidMount() {
+        const WINDOW_HEIGHT = 600;
+        const WINDOW_WIDTH = 800;
+
         let Engine = Matter.Engine,
         Render = Matter.Render,
         Runner = Matter.Runner,
@@ -14,17 +24,17 @@ class Project extends Component {
     
         // create a renderer
         var render = Render.create({
-            element: this.refs.ProjectDiv,
+            element: this.refs.GameDiv,
             engine: engine,
             options: {
-                width: 800,
-                height: 600,
-                //wireframes: false
+                width: WINDOW_WIDTH,
+                height: WINDOW_HEIGHT,
+                wireframes: false
             }
         });
     
         // create bodies
-        var ground = Bodies.rectangle(600, 350, 300, 20, { isStatic: true });
+        var ground = Bodies.rectangle(WINDOW_HEIGHT, 350, 300, 20, { isStatic: true });
 
         let ball = Matter.Bodies.circle(200,300,20);
         let sling = Matter.Constraint.create({
@@ -36,11 +46,12 @@ class Project extends Component {
         let stack = Matter.Composites.stack(500,140,10,10,0,0, (x,y)=>{
             return Matter.Bodies.rectangle(x,y,20,20,{
                 // render:{
-                //     fillStyle:'#156234',
-                //     strokeStyle:'orange',
-                // }
+                //     visible
+                // },
+                // wireframe: false
             });
         });
+        console.log(stack);
 
         //set mouse constraints
         let mouse = Matter.Mouse.create(render.canvas);
@@ -59,11 +70,20 @@ class Project extends Component {
             if(e.body === ball) firing = true;
         });
         Matter.Events.on(engine, 'afterUpdate', ()=>{
+            //Logic pertaining to the slingshot
             if(firing && Math.abs(ball.position.x-200)<20 && Math.abs(ball.position.y-300)<20){
                 ball = Matter.Bodies.circle(200, 300, 20);
                 Matter.World.add(engine.world,ball);
                 sling.bodyB = ball;
                 firing = false;
+            }
+
+            // Logic for calculating score
+            for(let i=0; i<stack.bodies.length; i++){
+                if(stack.bodies[i].position.y > WINDOW_HEIGHT){
+                    stack = Matter.Composite.remove(stack, stack.bodies[i]);
+                    this.setState({score: this.state.score+1});
+                }
             }
         });
     
@@ -83,7 +103,10 @@ class Project extends Component {
 
     render() {
         return (
-            <div className="align-center" ref="ProjectDiv"></div>
+            <div>
+                <h1>Your score is: {this.state.score}</h1>
+                <div className="align-center" ref="GameDiv"></div>
+            </div>
         );
     }
 }
