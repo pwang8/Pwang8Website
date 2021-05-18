ef8ea0448977dab9f5c5ccedd3b39fa74b2ac89e
Paul Wang <p.wang8@outlook.com>
2021-05-18 17:49:27 -0600

    Added post function

--- src/Components/Leaderboard.js
+++ src/Components/Leaderboard.js
@@ -6,7 +6,41 @@ class Leaderboard extends Component {
         this.state = {leaderboardEntries:[]};
     }
 
+    submitScore = (event) => {
+        event.preventDefault();
+        const API_URL = "https://nptgn9fou5.execute-api.us-east-1.amazonaws.com/postDBEntry"
+        const bodyString = JSON.stringify({
+            "name": event.target[0].value,
+            "score": this.props.score
+        });
 
+        fetch(API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: bodyString
+        }).catch(err => {
+            console.log(err);
+        });
+        console.log(bodyString);
+
+    }
+
+    sortEntriesByScore = (entriesArray) => {
+        for(let i=0; i<entriesArray.length; i++){
+            let maxIndex = i;
+            for(let j=i; j<entriesArray.length; j++){
+                if(entriesArray[j].Score > entriesArray[maxIndex].Score){
+                    maxIndex = j;
+                }
+            }
+            let temp = entriesArray[maxIndex];
+            entriesArray.splice(maxIndex,1);
+            entriesArray.splice(i, 0, temp);
+        }
+        return entriesArray;
+    }
 
     async componentDidMount() {
         const API_URL = 'https://nptgn9fou5.execute-api.us-east-1.amazonaws.com/getAllScores';
@@ -20,7 +54,8 @@ class Leaderboard extends Component {
             return res.json()
         })
         .then(data => {
-            this.setState({leaderboardEntries:data});
+            let sortedData = this.sortEntriesByScore(data);
+            this.setState({leaderboardEntries:sortedData});
         })
         .catch(err => {
             console.log(err);
@@ -39,10 +74,10 @@ class Leaderboard extends Component {
                     ))}
                 </ul>
                 <h1>Your score is: {this.props.score}</h1>
-                {/* <form>
-                    <input>Name</input>
-                    <button>Submit</button>
-                </form> */}
+                <form onSubmit={this.submitScore} >
+                    <input type="text" name="Name" placeholder="Name"/>
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         );
     }
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -6,9 +6,9 @@ class Portfolio extends Component {
     return (
       <section id="portfolio">
         <div className="row">
-            <h1>Currently working on a full-stack project (WIP)</h1>
-
             <div id="portfolio-wrapper" className="cf">
+              <h1>Project</h1>
+              <p>Scores are stored on AWS Dynamo-DB and displayed for all visitors of the site. I ask that you be respectful when submitting names alongside your score. It's a silly game but have fun!</p>
               <Project
                 //Default Values
                 defaultCanvasHeight = {600}
