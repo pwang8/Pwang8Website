1b58c3ca44355e05d244ae1a01452828c9ad9bd9
Paul Wang <p.wang8@outlook.com>
2022-06-16 21:33:56 -0600

    6-16 update to start applying with, wish me luck

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -8,7 +8,7 @@ class Portfolio extends Component {
         <div className="row">
             <div id="portfolio-wrapper" className="cf">
               <h1>Project</h1>
-              <p>Scores are stored on AWS Dynamo-DB and displayed for all visitors of the site. I ask that you be respectful when submitting names alongside your score. It's a silly game but have fun!</p>
+              <p>Scores are stored on AWS Dynamo-DB and displayed for all visitors of the site. I ask that you be respectful when submitting names alongside your score. It's just a silly game since I primarily just wanted an excuse to tinker around with AWS tech in the back-end but have fun!</p>
               <Project
                 //Default Values
                 defaultCanvasHeight = {600}
